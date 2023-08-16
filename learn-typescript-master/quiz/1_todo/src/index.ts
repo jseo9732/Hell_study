ce325c8c6daa8a82d3459f5a1ec4d23ce9f5224a
jisu Seo <jseo9732@gmail.com>
2023-08-16 15:05:48 +0900

    4.5 프로젝트 실습 풀이

--- learn-typescript-master/quiz/1_todo/src/index.ts
+++ learn-typescript-master/quiz/1_todo/src/index.ts
@@ -1,7 +1,13 @@
-let todoItems: { id: number; title: string; done: boolean }[];
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+let todoItems: Todo[];
 
 // api
-function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
+function fetchTodoItems(): Todo[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -11,40 +17,38 @@ function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
 }
 
 // crud methods
-function fetchTodos(): { id: number; title: string; done: boolean }[] {
+function fetchTodos(): Todo[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo: Todo): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index: number, todo: Todo): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo(): { id: number; title: string; done: boolean } {
+function logFirstTodo(): Todo {
   return todoItems[0];
 }
 
-function showCompleted(): { id: number; title: string; done: boolean }[] {
-  return todoItems.filter(
-    (item: { id: number; title: string; done: boolean }) => item.done
-  );
+function showCompleted(): Todo[] {
+  return todoItems.filter((item: Todo) => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems(todo1, todo2) {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
-  addTodo(todo1);
-  addTodo(todo2);
+  addTodo({ id: 4, title: '타입 정의', done: false });
+  addTodo({ id: 5, title: '복습', done: false });
 }
 
 // NOTE: 유틸 함수
